1f3b0938bf9702f667f136e8d3d5edf311479af5
Kirill <shirshov_k@se.zgrad.ru>
2026-06-23 17:09:52 +0300

    Add notes listing

--- app/main.py
+++ app/main.py
@@ -1,5 +1,28 @@
+import json
+import sys
+
+def add_note(text):
+    with open("data/notes.json", "r", encoding="utf-8") as f:
+        notes = json.load(f)
+
+        notes.append({"text": text})
+
+        with open("data/notes.json", "w", encoding="utf-8") as f:
+            json.dump(notes, f, ensure_ascii=False, indent=2)
+
 def main():
-    print("ADHD assistent started")
+
+    with open("data/notes.json", "r", encoding="utf-8") as f:
+        notes = json.load(f)
+    
+    text = "".join(sys.argv[1:])
+    
+    if text == "list":
+        for i, note in enumerate(notes,1):
+            print(f"{i}. {note.get('text', '')}")
+            
+    else:
+        add_note(text)
 
 if __name__ == "__main__":
     main()
