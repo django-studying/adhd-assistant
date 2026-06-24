8ffdf7248a32c280100c74d01d46b9f6523b5ed5
Kirill <shirshov_k@se.zgrad.ru>
2026-06-24 15:44:52 +0300

    Add filter and datetime create

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 import json
 import sys
 
-def add_note(text):
+def add_note(note_type, text, created_note):
     with open("data/notes.json", "r", encoding="utf-8") as f:
         notes = json.load(f)
 
-        notes.append({"text": text})
+        notes.append({"type": note_type, "text": text, "datetime": created_note})
 
         with open("data/notes.json", "w", encoding="utf-8") as f:
             json.dump(notes, f, ensure_ascii=False, indent=2)
@@ -15,18 +16,34 @@ def main():
     with open("data/notes.json", "r", encoding="utf-8") as f:
         notes = json.load(f)
     
-    text = "".join(sys.argv[1:])
-    
-    if text == "list":
-        filtered_notes = [note for note in notes if note.get('text', '').strip()]
-        for i, note in enumerate(filtered_notes, 1):
-            print(f"{i}. {note.get('text', '')}")
-    elif text == "clear":
+    note_type = "".join(sys.argv[1])
+    text = " ".join(sys.argv[2:])
+    created_note = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    filtered_notes = [note for note in notes if note.get('text', '').strip()]
+            
+    if note_type == "list":
+        if text == "task":
+            for i, note in enumerate(filtered_notes, 1):
+                if note.get('type', '').strip() == "task":
+                    print(f"{i}. [{note.get('type', '')}]: {note.get('text', '')}")
+        if text == "note":
+            for i, note in enumerate(filtered_notes, 1):
+                if note.get('type', '').strip() == "note":
+                    print(f"{i}. [{note.get('type', '')}]: {note.get('text', '')}")
+        if text == "idea":
+            for i, note in enumerate(filtered_notes, 1):
+                if note.get('type', '').strip() == "idea":
+                    print(f"{i}. [{note.get('type', '')}]: {note.get('text', '')}")
+        if text == "all" or text == "":
+            for i, note in enumerate(filtered_notes, 1):
+                print(f"{i}. [{note.get('type', '')}]: {note.get('text', '')}")        
+    elif note_type == "clear":
         notes = []
         with open("data/notes.json", "w", encoding="utf-8") as f:
             json.dump(notes, f, ensure_ascii=False, indent=2)
-    elif text.strip():
-        add_note(text)
+    elif text:
+        add_note(note_type=note_type, text=text, created_note=created_note)
 
 if __name__ == "__main__":
     main()
